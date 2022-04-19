efb086b8fd0cbf0a6931c366d66e7215a36b5e1f
EarlFrancis123 <92779010+EarlFrancis123@users.noreply.github.com>
2022-04-19 22:10:15 +0800

    Car Activity

--- constructors.java
+++ constructors.java
@@ -1,11 +1,15 @@
 public class constructors {
      public static void main(String [] args){
-        person person1 = new person("Earl","Francis", "IT");
 
+        person person1 = new person("Earl","Francis", "IT");
         System.out.println("My Name is" + person1.fname + person1.lname + "and my job is" + person1.job);
 
-        person person2 = new person("Jungie", "Gerez", "Programer");
-        System.out.println("My Name is" + person2.fname + person2.lname + "and my job is" + person2.job);
+        person person3 = new person("Jungie", "Gerez", "Programer");
+        System.out.println("My Name is" + person3.fname + person3.lname + "and my job is" + person3.job);
+        person2 Person2 = new person2("","", "");
+        Person2.setFname("John");
+
+        System.out.println(Person2.getFname());
         
     }
 }
--- person.java
+++ person.java
@@ -1,12 +1,19 @@
 public class person {
-    String fname;
-    String lname;
-    String job;
+  String fname;
+  String lname;
+  String job;
 
 public person(String fname, String lname, String job){
     this.fname = fname;
     this.lname = lname;
     this.job = job;
+
+
+}
+public person(String fname, String lname){
+    this.fname = fname;
+    this.lname = lname;
+
 
 
 }
